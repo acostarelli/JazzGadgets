0477431cb1c099abf65085b049303a63ef7c10db
SirPython <SirPython@users.noreply.github.com>
2018-10-01 16:26:25 -0400

    talk about simplification

--- jazzgadgets.user.js
+++ jazzgadgets.user.js
@@ -1,55 +1,52 @@
 // ==UserScript==
-// @name         JazzGadgets
-// @namespace    https://github.com/SirPython/JazzGadgets
+// @name         Jazz Gadgets.
 // @version      0.1
-// @description  Jazz
+// @description  A transcription tool.
 // @author       SirPython
 // @match        *://www.youtube.com/watch?v=*
-// @grant        GM_getResourceText
-// @resource     actionPanel https://raw.githubusercontent.com/SirPython/JazzGadgets/master/resources/action-panel.html
+// @grant        none
 // ==/UserScript==
 
-const injectActionPanel = () => {
-	const button = document.createElement("button");
-	button.className = "yt-uix-button yt-uix-button-size-default yt-uix-button-opacity yt-uix-button-has-icon no-icon-markup pause-resume-autoplay action-panel-trigger action-panel-trigger-share   yt-uix-tooltip";
-	button.type = "button";
-	button.onclick = "; return false;";
-	button.title = "JazzGadgets";
-	button.dataset.triggerFor = "action-panel-jazzgadgets";
-	button.dataset.buttonToggle = true;
-	
-	const span = document.createElement("span");
-	span.className = "yt-uix-button-content";
-	span.innerText = "JazzGadgets";
-	button.appendChild(span);
-	
-	const actionPanel = document.createElement("div");
-	actionPanel.id = "action-panel-jazzgadgets";
-	actionPanel.className = "action-panel-content hid";
-	actionPanel.dataset.panelLoaded = true;
-	actionPanel.innerHTML = GM_getResourceText("actionPanel");
-	
-	document.getElementById("watch8-secondary-actions").appendChild(button);
-	document.getElementById("watch-action-panels").appendChild(actionPanel);
-	
-	return actionPanel.children[0];
-};
-
-const setLoop = ({times}) => {
-	
-};
-
-const setSpeed = ({speed}) => {
-	
-};
-
-const setVolume = ({volume}) => {
-	
-}
+/**
+ * Instructions:
+ *
+ * i   --> set landmark in video timeline
+ * o   --> jump to landmark
+ * 1-9 --> playback speed
+ * 0   --> reset playback speed
+ *
+ * Hold down ctrl when doing the numbers to prevent YT's features.
+ */
 
 (function() {
-    'use strict';
-	const form = injectActionPanel();
-	
-	
+    const video = document.getElementsByClassName("html5-main-video")[0];
+    let start = 0;
+
+    document.onkeyup = ({key}) => {
+        switch(key) {
+            case 'i':
+                start = video.currentTime;
+                break;
+
+            case 'o':
+                video.currentTime = start;
+                break;
+
+            case '0':
+                video.playbackRate = 1;
+                break;
+
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                video.playbackRate = (+key) / 10;
+                break;
+        }
+    }
 })();

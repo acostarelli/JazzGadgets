39964d989a5984e92963865eaee2c2ce2e7f5c24
SirPython <SirPython@users.noreply.github.com>
2020-06-07 14:06:24 -0400

    works again now; working on implementing recording functionality

--- jazzgadgets.user.js
+++ jazzgadgets.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Jazz Gadgets.
+// @name         Jazz Gadgets
 // @version      0.1
 // @description  A transcription tool.
 // @author       SirPython
@@ -7,29 +7,50 @@
 // @grant        none
 // ==/UserScript==
 
-/**
- * Instructions:
- *
- * i   --> set landmark in video timeline
- * o   --> jump to landmark
- * 1-9 --> playback speed
- * 0   --> reset playback speed
- *
- * Hold down ctrl when doing the numbers to prevent YT's features.
- */
-
 (function() {
+    class Enum {
+        constructor(...args) {
+            for(const arg of args) {
+                this[arg] = Symbol(arg);
+            }
+        }
+    }
+
     const video = document.getElementsByClassName("html5-main-video")[0];
     let start = 0;
+    let rate = 0;
+
+    let paused = false;
+    let vol = null;
+    let now = null;
+
+    const RecordingState = new Enum("NOT_RECORDING", "RECORDING", "PLAYBACK");
+    const recState = RecordingState.NOT_RECORDING;
+    const rec = null;
+    const audio = null;
+    const playbackInt = null;
 
     document.onkeyup = ({key}) => {
         switch(key) {
-            case 'i':
+            case 'u':
                 start = video.currentTime;
                 break;
 
             case 'o':
                 video.currentTime = start;
+                break;
+
+            case 'n':
+                if(!paused) {
+                    now = video.currentTime;
+                    vol = video.volume;
+                    video.volume = 0
+                    paused = true
+                } else {
+                    video.currentTime = now;
+                    video.volume = vol;
+                    paused = false;
+                }
                 break;
 
             case '0':
@@ -47,6 +68,46 @@
             case '9':
                 video.playbackRate = (+key) / 10;
                 break;
+
+            case 'r':
+                switch(recState) {
+                    case RecordingState.NOT_RECORDING:
+                        navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+                            rec = new MediaRecorder(stream);
+                            const chunks = [];
+
+                            rec.start();
+                            rec.addEventListener("dataavailable", e => chunks.push(e.data));
+                            rec.addEventListener("stop", _ => {
+                                audio = new Audio(
+                                    URL.createObjectURL(
+                                        new Blob(chunks)
+                                    )
+                                )
+                            });
+                        });
+                        break;
+                    case RecordingState.RECORDING: {
+                        rec.stop();
+
+                        const recTime = video.currentTime;
+                        video.currentTime = start;
+
+                        const playback = () => {
+                            while(video.currentTime < recTime);
+
+                            audio.play()
+
+                            playbackInt = setTimeout(playback, 0);
+                        }
+
+                        playbackInt = setTimeout(playback, 0);
+                        break;
+                    }
+                    case RecordingState.PLAYBACK:
+                        break;
+                }
+
         }
     }
 })();
